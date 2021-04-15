8ec8e176d6f852d2126688b32e286b85d4b79672
tate <tate@localhost>
2021-04-15 16:55:08 -0700

    Custom_Array_List is complete! Yooooooooooo oh wait I only started it today

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,33 +3,59 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	private T[] list;
 	public ArrayList() {
+		list  = (T[])new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return list[loc];
 	}
 	
 	public void add(T val) {
-		
+		 T[] newList = (T[])new Object[list.length+1];
+		 for(int i = 0; i<list.length; i++) {
+			 newList[i]=list[i];
+		 }
+		 newList[newList.length-1]=val;
+		 list=newList;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] newList = (T[])new Object[list.length+1];
+		for(int i = 0; i<loc; i++) {
+			 newList[i]=list[i];
+		}
+		newList[loc]=val;
+		for(int i = loc; i<list.length; i++) {
+			newList[i+1]=list[i];
+		}
+		list=newList;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		list[loc]=val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] newList = (T[])new Object[list.length-1];
+		for(int i = 0; i<loc; i++) {
+			 newList[i]=list[i];
+		}
+		for(int i = loc+1; i<list.length; i++) {
+			newList[i-1]=list[i];
+		}
+		list=newList;
 	}
 	
 	public boolean contains(T val) {
-		
+		for(int i = 0; i<list.length; i++) {
+			if(list[i]==val) {
+				return true;
+			}
+		}
 		return false;
 	}
+	public int size() {return list.length;}
 }

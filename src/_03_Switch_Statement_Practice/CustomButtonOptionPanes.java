c5a450dc60b472401f6f2abeb1741f5a748a1095
tate <tate@localhost>
2021-04-22 17:00:45 -0700

    Horoscope(ip)

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,33 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
-		
+		switch(choice) {
+		case "Sunday":
+			JOptionPane.showMessageDialog(null, "Hope your weekend was good!");
+		break;
+		case "Monday":
+			JOptionPane.showMessageDialog(null, "*Yawn!* Mondays, amirite?");
+			JOptionPane.showMessageDialog(null, "Anyway, got any lasagna?");
+		break;
+		case "Tuesday":
+			JOptionPane.showMessageDialog(null, "Welp. Good luck!");
+		break;
+		case "Wednesday":
+			JOptionPane.showMessageDialog(null, "Oh");
+		break;
+		case "Thursday":
+			JOptionPane.showMessageDialog(null, "Oh, same!");
+		break;
+		case "Friday":
+			JOptionPane.showMessageDialog(null, "Have a nice weekend!");
+		break;
+		case "Saturday":
+			JOptionPane.showMessageDialog(null, "That's nice.");
+		break;
+		default:
+			JOptionPane.showMessageDialog(null, "Wait.");
+			JOptionPane.showMessageDialog(null, "That's illegal!");
+		break;
+		}
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -8,7 +8,49 @@ public class _00_Horoscope {
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+	void scopeOfTheDay(Zodiac sign) {
+		switch(sign) {
+		case ARIES:
+			JOptionPane.showMessageDialog(null, "Expect ups and downs in your future. You will see trees today.");
+		break;
+		case TAURUS:
+			JOptionPane.showMessageDialog(null, "Your freinds will stand by you today, but remember them in their times of need.");
+		break;
+		case GEMINI:
+			JOptionPane.showMessageDialog(null, "Today is twin day. Hope you got a matching outfit!");
+		break;
+		case CANCER:
+			JOptionPane.showMessageDialog(null, "Today is a lucky day for you! Your objectives will be fufilled.");
+		break;
+		case LEO:
+			JOptionPane.showMessageDialog(null, "Today, it is best to stay home. nothing outside will give you what you wish.");
+		break;
+		case VIRGO:
+			JOptionPane.showMessageDialog(null, "Hope for the best today. It is all you can do until tomorrow.");
+		break;
+		case LIBRA:
+			JOptionPane.showMessageDialog(null, "You should pursue your passions today, for it is better than ever to do so.");
+		break;
+		case SCORPIO:
+			JOptionPane.showMessageDialog(null, "nah.");
+		break;
+		case SAGITTARIUS:
+			JOptionPane.showMessageDialog(null, "Your day will be very productive, and you are able to get plenty of work done.");
+		break;
+		case CAPRICORN:
+			JOptionPane.showMessageDialog(null, "Your day will be an eventful one, and filled with excitement.");
+		break;
+		case AQUARIUS:
+			JOptionPane.showMessageDialog(null, "Everything today will go well for you.");
+		break;
+		case PISCES:
+			JOptionPane.showMessageDialog(null, "Fish! Very good. *clap clap* Also, check the obituaries soon. Someone important will die today.");
+		break;
+		}
+	}
 	// 3. Make a main method to test your method
-	
+	public static void main(String[] args) {
+		_00_Horoscope h = new _00_Horoscope();
+		scopeOfTheDay(h);
+	}
 }
